3a6657f8cb44a049b50bb9a76ccf0a17941d5160
ecuasonic <ecuas@lawn-10-91-60-60.lawn.gatech.edu>
2024-07-01 14:22:57 -0400

    lock script changed

--- custom_scripts/.config/custom_scripts/lock.sh
+++ custom_scripts/.config/custom_scripts/lock.sh
@@ -7,7 +7,7 @@ then
 fi
 scrot /tmp/screen.png
 # convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG -blur 0x8 $TMPBG
+magick $TMPBG -blur 0x8 $TMPBG
 # convert $TMPBG -blur 0x8 $TM
 # convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -i $TMPBG
+i3lock -i $TMPBG -ef
---
+++ custom_scripts/.config/custom_scripts/lock_suspend.sh
@@ -0,0 +1,1 @@
+~/.cargo/bin/xidlehook --not-when-fullscreen --not-when-audio --timer 600 '$(~/.config/custom_scripts/lock.sh)' '' --timer 900 'systemctl suspend' ''&
