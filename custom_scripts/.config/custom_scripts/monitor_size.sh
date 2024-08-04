e0bc56c0e4df71f1bd9bf276d9cee17ffc1260a9
ecuasonic <ecuasonic@gmail.com>
2024-08-04 19:09:50 -0400

    added control condition for background/display

--- custom_scripts/.config/custom_scripts/monitor_size.sh
+++ custom_scripts/.config/custom_scripts/monitor_size.sh
@@ -4,6 +4,8 @@ if [ $display_ports = "HDMI-1-1" ]; then
     SIZE="1920x1080"
 elif [ $display_ports = "eDP-2" ] || [ $display_ports = "eDP-1" ]; then
     SIZE="2560x1600"
+elif [ $display_ports = "LVDS-1" ]; then
+    SIZE="1440x900"
 fi
 
 echo $SIZE
