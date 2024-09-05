091df8a7ce9e576404949aa896a38cb6e75b20b8
ecuasonic <ecuasonic@gmail.com>
2024-09-04 21:29:46 -0400

    indent-blankline.lua: same settings

--- nvim/.config/nvim/lua/ecuas/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/ecuas/plugins/indent-blankline.lua
@@ -1,8 +1,18 @@
+local highlight = {
+    "CursorColumn",
+    "Whitespace",
+}
+
 return {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPre", "BufNewFile" },
     main = "ibl",
-    opts = {
-        indent = { char = "┊" },
-    },
+    config = function()
+        require("ibl").setup {
+            enabled = true,
+            indent = { char = "┊" },
+            whitespace = { highlight = { "Whitespace", "NonText" } },
+            scope = { enabled = true },
+        }
+    end,
 }
