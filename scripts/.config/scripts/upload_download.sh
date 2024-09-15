d48b0dcd1ffcfe87759420aaac006810e9d20d6c
ecuasonic <ecuasonic@gmail.com>
2024-09-15 02:14:58 -0400

    Download notes from google drive

--- scripts/.config/scripts/upload_download.sh
+++ scripts/.config/scripts/upload_download.sh
@@ -3,18 +3,22 @@
 dir="$HOME/dotfiles/scripts/.config/scripts/rofi"
 rofi_command="rofi -no-config -hover-select -theme $dir/screenshot.rasi"
 
-notes="Upload Notes"
+upload_notes="Upload Notes"
+download_notes="Download Notes"
 pictures="Upload Pictures"
 goodnotes="Download Goodnotes"
 
-prompt="$notes\n$pictures\n$goodnotes"
+prompt="$upload_notes\n$pictures\n$download_notes\n$goodnotes"
 chosen="$(echo -e "$prompt" | $rofi_command -p "Data Transfer" -dmenu -selected-row 0)"
 case $chosen in
-    $notes)
+    $upload_notes)
         rclone sync ~/Documents/Obsidian_Notes remote-google-drive:notes --drive-use-trash=false
         ;;
     $pictures)
         rclone sync ~/Pictures remote-google-drive:photos --drive-use-trash=false
+        ;;
+    $download_notes)
+        rclone copy remote-google-drive:notes ~/Documents/Obsidian_Notes
         ;;
     $goodnotes)
         rclone copy remote-google-drive:GN_Rough_Images ~/Documents/Goodnotes
