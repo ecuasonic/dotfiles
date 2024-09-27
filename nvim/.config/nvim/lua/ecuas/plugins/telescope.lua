decca55dd56ef31b08619b5fb36154a0caf4ce0a
ecuasonic <ecuasonic@gmail.com>
2024-09-27 13:18:42 -0400

    Telescope keybinds

--- nvim/.config/nvim/lua/ecuas/plugins/telescope.lua
+++ nvim/.config/nvim/lua/ecuas/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    tag = "0.1.8",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "sharkdp/fd",
@@ -11,51 +11,58 @@ return {
     config = function()
         local telescope = require("telescope")
         local actions = require("telescope.actions")
-        local transform_mod = require("telescope.actions.mt").transform_mod
-
-        -- local trouble = require("trouble")
-        -- local trouble_telescope = require("trouble.sources.telescope")
-
-        -- or create your custom action
-        -- local custom_actions = transform_mod({
-        --     open_trouble_qflist = function(prompt_bufnr)
-        --         trouble.toggle("quickfix")
-        --     end,
-        -- })
 
         telescope.setup({
             defaults = {
-                path_display = { "smart" },
+                file_ignore_patterns = {
+                    "%.png",
+                    "%.jpg",
+                    "%.pdf",
+                    ".gitignore",
+                    "/obj/",
+                    "/bin/",
+                },
+                layout_strategy = 'vertical',
+                layout_config = {
+                    height = 0.95,
+                },
+                path_display = {
+                    "smart"
+                },
                 mappings = {
                     i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            -- ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            -- ["<C-t>"] = trouble_telescope.open,
-                    }
+                        ["<ESC>"] = actions.close,
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-j>"] = actions.move_selection_next,
+
+                        ["<C-u>"] = actions.preview_scrolling_up,
+                        ["<C-d>"] = actions.preview_scrolling_down,
+
+                        ["<C-/>"] = actions.which_key,
+                    },
                 }
-            }
+            },
         })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Fuzzy Find" })
         vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Fuzzy Git" })
-        vim.keymap.set('n', '<leader>fcw', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end, { desc = "Fuzzy Current Word (within other words)" })
 
-        vim.keymap.set('n', '<leader>fcW', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end, { desc = "Fuzzy Current Word (separate from other words)" })
+        vim.keymap.set('v', '<leader>fs', function()
+            local reg_unnamed = vim.fn.getreg('""')
+            vim.cmd('normal! "zy')
+            local selected_text = vim.fn.getreg('z')
+            vim.fn.setreg('""', reg_unnamed)
+            builtin.grep_string({ search = selected_text })
+        end, {desc = "Fuzzy Current Selected Text"})
 
         vim.keymap.set('n', '<leader>fs', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            builtin.grep_string({ search = vim.fn.input("Search > ") })
         end, {desc = "Fuzzy Search" })
 
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Fuzzy Help Tags" })
+        vim.keymap.set('n', '<leader>fh', builtin.search_history, { desc = "Telescope Search History" })
         vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = "Fuzzy find recent files" })
-        vim.keymap.set('n', '<leader>ft', "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+        vim.keymap.set('n', '<leader>ft', builtin.tags, { desc = "Telescope tags" })
+        vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = "Telescope jumplist" })
     end
 }
