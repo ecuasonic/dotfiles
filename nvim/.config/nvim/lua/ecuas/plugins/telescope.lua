83107897ae920eee63b120428e69722cfde45ec2
ecuasonic <ecuasonic@gmail.com>
2024-09-30 16:02:08 -0400

    Telescope colors changed

--- nvim/.config/nvim/lua/ecuas/plugins/telescope.lua
+++ nvim/.config/nvim/lua/ecuas/plugins/telescope.lua
@@ -1,23 +1,28 @@
+local second_color = "#202020"
 vim.api.nvim_create_autocmd("Filetype", {
     pattern = "TelescopeResults",
     callback = function(ctx)
         vim.api.nvim_buf_call(ctx.buf, function()
-            vim.fn.matchadd("text", "^.*:[0-9]*:[0-9]*:")
-            vim.fn.matchadd("text", '[^:\\[\\]\\(\\) ]*/')
-            vim.api.nvim_set_hl(0, "text", { fg = "#404040" })
+            vim.fn.matchadd("color", '| [^/]*/')
+            vim.api.nvim_set_hl(0, "color", { fg = second_color })
+
+            vim.fn.matchadd("white", "[^/]*:.*:.*/")
+            vim.api.nvim_set_hl(0, "white", { fg = "#FFFFFF" })
+
+            vim.fn.matchadd("recolor", "| .*:[0-9]*:[0-9]*:")
+            vim.api.nvim_set_hl(0, "recolor", { fg = second_color })
         end)
     end,
 })
 
-local function live_grep_change(_, path)
+local function filename(_, path)
     local tail = vim.fs.basename(path)
     local parent = vim.fs.dirname(path)
     if parent == "." then
-        return tail
+        return string.format("| %s", tail)
     else
-        return string.format("%s/%s", parent, tail)
+        return string.format("| %s/%s", parent, tail)
     end
-    return path
 end
 
 return {
@@ -36,6 +41,15 @@ return {
 
         telescope.setup({
             defaults = {
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                },
                 file_ignore_patterns = {
                     "%.png",
                     "%.jpg",
@@ -44,6 +58,8 @@ return {
                     "%/obj/%",
                     "%/bin/%",
                 },
+                color_devicons = false,
+                selection_caret = ' ',
                 sorting_strategy = 'descending',
                 selection_strategy = 'row',
                 scroll_strategy = 'limit',
@@ -55,9 +71,7 @@ return {
                 layout_config = {
                     height = 0.95,
                 },
-                path_display = {
-                    shorten = 7
-                },
+                path_display = filename,
                 mappings = {
                     i = {
                         ["<ESC>"] = actions.close,
@@ -85,9 +99,8 @@ return {
             builtin.grep_string({ search = selected_text })
         end, {desc = "Fuzzy Current Selected Text"})
 
-        vim.keymap.set('n', '<leader>fs', builtin.live_grep)
+        vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = "Fuzzy Text"})
         vim.keymap.set('n', '<leader>fh', builtin.search_history, { desc = "Telescope Search History" })
-        vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = "Fuzzy find recent files" })
         vim.keymap.set('n', '<leader>ft', builtin.tags, { desc = "Telescope tags" })
         vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = "Telescope jumplist" })
     end
