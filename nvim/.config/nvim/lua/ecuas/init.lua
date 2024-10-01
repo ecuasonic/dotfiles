8845d84fabbfffb7c9707ef1328e74ab0f39d344
ecuasonic <ecuasonic@gmail.com>
2024-09-30 22:12:49 -0400

    LSP + Compile_Commands + Bear

--- nvim/.config/nvim/lua/ecuas/init.lua
+++ nvim/.config/nvim/lua/ecuas/init.lua
@@ -31,7 +31,7 @@ autocmd('TextYankPost', {
     callback = function()
         vim.highlight.on_yank({
             higroup = 'IncSearch',
-            timeout = 40,
+            timeout = 100,
         })
     end,
 })
@@ -44,15 +44,12 @@ autocmd('LspAttach', {
     group = ecuasGroup,
     callback = function(e)
         local opts = { buffer = e.buf }
-        vim.keymap.set("n", "<c-d>", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "d]", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "d[", function() vim.diagnostic.goto_prev() end, opts)
-        -- vim.keymap.set("n", "gd", function() vim.lsp.buf.defintion() end, opts)
-        -- vim.keymap.set("n", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
-        -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.reference() end, opts)
-        -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.renamedefintiondefintion() end, opts)
+        vim.keymap.set("n", "gs", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.declaration() end, opts)
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.definition() end, opts)
     end,
 })
 
