12f105ec3ee9abefccee50b3ede25189c99545bc
ecuasonic <ecuasonic@gmail.com>
2024-12-04 18:01:56 -0500

    i3 Mouse warping

---
+++ scripts/.config/scripts/mouse_warping.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+while getopts "mfhjkl" option; do
+    case $option in
+        m)
+            command="move"
+            ;;
+        f)
+            command="focus"
+            ;;
+        h)
+            command+=" left"
+            ;;
+        j)
+            command+=" down"
+            ;;
+        k)
+            command+=" up"
+            ;;
+        l)
+            command+=" right"
+            ;;
+        *)
+            ;;
+    esac
+done
+
+i3-msg "$command"
+
+eval "$(xdotool getwindowfocus getwindowgeometry --shell)"
+xdotool mousemove $((X + WIDTH / 2)) $((Y + HEIGHT / 2))
