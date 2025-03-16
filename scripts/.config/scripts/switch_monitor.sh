f444996ea4b4b17a7c2d295387fa6c567c717299
ecuasonic <ecuasonic@gmail.com>
2025-03-16 14:52:38 -0400

    startup search for monitor (thinkpad)

--- scripts/.config/scripts/switch_monitor.sh
+++ scripts/.config/scripts/switch_monitor.sh
@@ -18,28 +18,26 @@ restart_nitrogen_i3() {
 }
 
 main() {
-    if [ "$display_ports" = "HDMI-1-1" ]; then
-
+    # HDMI -> eDP
+    if [ "$display_ports" = "HDMI-1-1" ] || [ "$display_ports" = "HDMI-1" ] || [ "$display_ports" = "HDMI-2" ]; then
+        xrandr --output eDP-1 --auto --output eDP-1 --same-as "$display_ports" --output "$display_ports" --off
         xrandr --output eDP-2 --auto --output eDP-2 --same-as "$display_ports" --output "$display_ports" --off
         sleep 0.5
         xrandr -r 60
 
         restart_nitrogen_i3
 
+    # eDP -> HDMI
     elif [ "$display_ports" = "eDP-2" ] || [ "$display_ports" = "eDP-1" ]; then
-
         xrandr --output HDMI-1-1 --auto --output HDMI-1-1 --same-as "$display_ports"
+        xrandr --output HDMI-1 --auto --output HDMI-1-1 --same-as "$display_ports"
+        xrandr --output HDMI-2 --auto --output HDMI-1-1 --same-as "$display_ports"
         display_ports2=$(~/.config/scripts/detect_monitor.sh)
-
         if [ "$display_ports2" != "eDP-2" ] && [ "$display_ports2" != "eDP-1" ]; then
-
             xrandr --output "$display_ports" --off
             restart_nitrogen_i3
-
         else
-
             xrandr -r 60
-
         fi
 
     fi
