bca2f2c43df4278749a718f2a25b3aab2deffa4f
ecuasonic <ecuasonic@gmail.com>
2025-05-29 00:15:16 -0400

    machine_specific updated

--- scripts/.config/scripts/startup/machine_specific.sh
+++ scripts/.config/scripts/startup/machine_specific.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-THINKPAD='de06baa0779d4e27866c8ef506f6dd59'
+id_hash=$(sha256sum /etc/machine-id | awk '{print $1}')
 
-if grep -q "$THINKPAD" /etc/machine-id; then
+THINKPAD_hash='10e5fa2ff74be5a896c3e2bc9637c8761e32fe94b95388738ed37f3e541c1f9d'
+G14_hash='9c4df82c32b181edd16f1050254ae731e49f46e63545df81a31cfcfd49fe6efe'
+
+if [[ "$id_hash" == "$THINKPAD_hash" ]]; then
     xinput disable "TPPS/2 IBM TrackPoint"
 fi
