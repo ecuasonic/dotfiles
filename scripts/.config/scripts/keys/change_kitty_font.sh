68ebd85bdc0b3598a4627ce3171718d92210bfd5
ecuasonic <ecuasonic@gmail.com>
2025-05-31 21:51:23 -0400

    fixed kitty-script

--- scripts/.config/scripts/keys/change_kitty_font.sh
+++ scripts/.config/scripts/keys/change_kitty_font.sh
@@ -3,25 +3,21 @@
 CONFIG_FILE=~/.config/kitty/kitty.conf
 CURRENT_SIZE=$(grep "font_size" $CONFIG_FILE | awk '{print $2}')
 
-function main() {
-    if (( $# == 0 )); then
-        echo "Current Font Size: $CURRENT_SIZE"
-    elif (( $# == 1 )); then
-        # Replace the old font size with the new font size in the configuration file
-        sed -i "s/font_size[[:space:]]*$CURRENT_SIZE/font_size $1/" "$CONFIG_FILE"
-        if pids=$(pidof kitty); then
-            kill -SIGUSR1 $pids
-        else
-            echo "Error: Kitty is not running."
-        fi
-        # Print a message indicating the change
-        sleep 0.1
-        # clear
-        echo "New Font Size: $1"
+if (( $# == 0 )); then
+    echo "Current Font Size: $CURRENT_SIZE"
+elif (( $# == 1 )); then
+    # Replace the old font size with the new font size in the configuration file
+    sed -i "s/font_size[[:space:]]*$CURRENT_SIZE/font_size $1/" "$CONFIG_FILE"
+    if pids=$(pidof kitty); then
+        kill -SIGUSR1 $pids
     else
-        echo "No parameters = show current font size"
-        echo "One parameter = change font size"
+        echo "Error: Kitty is not running."
     fi
-}
-
-main "$1"
+    # Print a message indicating the change
+    sleep 0.1
+    # clear
+    echo "New Font Size: $1"
+else
+    echo "No parameters = show current font size"
+    echo "One parameter = change font size"
+fi
